5303c7982c9653e0e3a8f4f9923c76fb70db6c14
ThStock <stock.th@gmail.com>
2014-07-30 17:33:52 +0200

    Added thymeleaf as java template engine (html is viewable)

--- src/main/scala/ExampleController.scala
+++ src/main/scala/ExampleController.scala
@@ -1,16 +1,34 @@
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.web.bind.annotation.{RequestMapping, RestController}
+import org.springframework.context.annotation.{ComponentScan, Configuration}
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
+import scala.collection.JavaConversions._
 
-object SpringStart extends App {
-  // $ mvn spring-boot:run # http://localhost:8080/
-  SpringApplication.run(classOf[ExampleController])
-}
+import scala.beans.BeanProperty
 
-@RestController
+@Controller
 @EnableAutoConfiguration
 class ExampleController {
 
   @RequestMapping(Array("/"))
-  def home() = "Hello World with scala 2.11!";
+  def home(): ModelAndView = {
+
+    case class Street(@BeanProperty name: String, @BeanProperty number: String)
+    val anyStreet = Street("Any Street", "32a")
+
+    view("index", Map("street" -> anyStreet))
+  }
+
+  private def view(name: String, model: java.util.Map[String, AnyRef]): ModelAndView = {
+    new ModelAndView(name, model)
+  }
+}
+
+@Configuration
+@ComponentScan
+object SpringStart extends App {
+  // $ mvn spring-boot:run -Dserver.port=8090 # http://localhost:8090/
+  SpringApplication.run(classOf[ExampleController])
 }
